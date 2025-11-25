8d855d643747754942836ecdd9e9598161ddef5c
Qualitech-conseil <215650589+Qualitech-conseil@users.noreply.github.com>
2025-11-25 14:06:55 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,8 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+    "https://vawvmiosgslvykfqxffs.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhd3ZtaW9zZ3NsdnlrZnF4ZmZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NjcxNTAsImV4cCI6MjA3OTU0MzE1MH0.T_q5fT1PCOt_pwEFdoKqePFYp7N4IXZSN1XA3HGG5v8"
+);
+
 
